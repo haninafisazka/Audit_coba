39b9e2e9b790ab973923ccea819987b6e794ac1b
dianaalbs <dianaadilla@student.uns.ac.id>
2023-07-11 10:39:35 +0700

    standar

--- resources/views/auditor/standar/standar.blade.php
+++ resources/views/auditor/standar/standar.blade.php
@@ -152,84 +152,89 @@
                 </ul>
             </div>
         </div>
-        <div class="col">
-            <h2 class="text-center">Hasil Evaluasi</h2>
-            <hr>
-            <div class="card p-3">
-                <form method="POST" action="/auditor/change_password">
-                    @csrf
-                    <div class="form-group row mb-3">
-                        <label for="password" class="col-md-0 col-form-label text-md-right">Kondisi Awal : </label>
+        <div class="col-10 border-start">
+{{--            CONTENT--}}
+            <div class="container-fluid">
+                <h1 class="fw-bold mt-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                        <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                        <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+                    </svg>
+                    TAMBAH PERIODE </h1>
+                <hr>
+                <div class="card mt-4 mb-3">
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('tambahPeriodeAudit') }}" enctype="multipart/form-data">
+                            @csrf
+                            <div class="form-group row mt-3 mb-3">
+                                <label for="tanggal_awal_audit" class="col-md-4 col-form-label text-md-right">{{ __('Tanggal Awal Audit') }}</label>
 
-                        <div class="col-12">
-                            <textarea name="description" class="form-control @error('description') is-invalid @enderror" aria-describedby="description" id="description" rows="3" >{{ old('description') }}</textarea>
-                            @error('description')
-                                <div id="description" class="invalid-feedback">
-                                    Deskripsi wajib diisi
+                                <div class="col-md-6">
+                                    <input type="date" class="form-control" name="tanggal_awal_audit" autofocus required>
                                 </div>
-                            @enderror
-                        </div>
-                    </div>
+                            </div>
 
-                    <div class="form-group row mb-3">
-                        <label for="password" class="col-md-0 col-form-label text-md-right">Dasar Evaluasi :</label>
+                            <div class="form-group row mt-3 mb-3">
+                                <label for="tanggal_akhir_audit" class="col-md-4 col-form-label text-md-right">{{ __('Tanggal Akhir Audit') }}</label>
 
-                        <div class="col-12">
-                            <textarea name="description" class="form-control @error('description') is-invalid @enderror" aria-describedby="description" id="description" rows="3" >{{ old('description') }}</textarea>
-                            @error('description')
-                                <div id="description" class="invalid-feedback">
-                                    Deskripsi wajib diisi
+                                <div class="col-md-6">
+                                    <input type="date" class="form-control" name="tanggal_akhir_audit" autofocus required>
                                 </div>
-                            @enderror
-                        </div>
-                    </div>
+                            </div>
 
-                    <div class="form-group row mb-3">
-                        <label for="password" class="col-md-0 col-form-label text-md-right">Penyebab : </label>
+                            <div class="form-group row mt-3 mb-3">
+                                <label for="no_sk_tugas_audit" class="col-md-4 col-form-label text-md-right">{{ __('Nomor SK Tugas Audit') }}</label>
 
-                        <div class="col-12">
-                            <textarea name="description" class="form-control @error('description') is-invalid @enderror" aria-describedby="description" id="description" rows="3" >{{ old('description') }}</textarea>
-                            @error('description')
-                                <div id="description" class="invalid-feedback">
-                                    Deskripsi wajib diisi
+                                <div class="col-md-6">
+                                    <input id="no_sk_tugas_audit" type="text" class="form-control" name="no_sk_tugas_audit" autofocus required>
                                 </div>
-                            @enderror
-                        </div>
-                    </div>
+                            </div>
 
- 
+                            <div class="form-group row mt-3 mb-3">
+                                <label for="file_sk" class="col-md-4 col-form-label text-md-right">{{ __('File SK') }}</label>
 
-                    <div class="form-group row mb-3">
-                        <label for="password" class="col-md-0 col-form-label text-md-right">Akibat : </label>
-
-                        <div class="col-12">
-                            <textarea name="description" class="form-control @error('description') is-invalid @enderror" aria-describedby="description" id="description" rows="3" >{{ old('description') }}</textarea>
-                            @error('description')
-                                <div id="description" class="invalid-feedback">
-                                    Deskripsi wajib diisi
+                                <div class="col-md-6">
+                                    <input type="file" class="form-control" name="file_sk" autofocus required>
                                 </div>
-                            @enderror
-                        </div>
-                    </div>
+                            </div>
 
+                            <div class="form-group row mt-3 mb-3">
+                                <label for="tanggal_sk" class="col-md-4 col-form-label text-md-right">{{ __('Tanggal SK') }}</label>
 
-                    div class="form-group row mb-3">
-                        <label for="password" class="col-md-0 col-form-label text-md-right">Rekomendasi Follow-Up : </label>
-
-                        <div class="col-12">
-                            <textarea name="description" class="form-control @error('description') is-invalid @enderror" aria-describedby="description" id="description" rows="3" >{{ old('description') }}</textarea>
-                            @error('description')
-                                <div id="description" class="invalid-feedback">
-                                    Deskripsi wajib diisi
+                                <div class="col-md-6">
+                                    <input type="date" class="form-control" name="tanggal_sk" autofocus required>
                                 </div>
-                            @enderror
-                        </div>
+                            </div>
+
+                            <div class="form-group row mt-3 mb-3">
+                                <label for="ketua_spi" class="col-md-4 col-form-label text-md-right">{{ __('Ketua SPI') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="ketua_spi" type="text" class="form-control" name="ketua_spi" required autofocus>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mt-3 mb-3">
+                                <label for="nip_ketua_spi" class="col-md-4 col-form-label text-md-right">{{ __('NIP Ketua SPI') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="nip_ketua_spi" type="text" class="form-control" name="nip_ketua_spi" required autofocus>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Submit') }}
+                                    </button>
+                                    <a href="{{ url()->previous() }}">
+                                        <button type="button" class="btn btn-secondary"> {{ __('Batal') }}</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-
-
-                    
-
-                
+                </div>
             </div>
         </div>
     </div>
